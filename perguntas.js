a9df5b9dcbd5d3e3134c7bc1ef3d1de926775486
Ashzys-flavio <00001106301808sp@aluno.educacao.sp.gov.br>
2024-11-26 09:12:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'O russo Yuri Gagarin (1934-1968).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Paraguai?',
+    'Assunção é a capital do Paraguai.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos tem o corpo humano bebe?',
+    ' esqueleto de um bebê tem cerca de 270 ossos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Ccomo se diz por nada em inglês?',
+    'Por nada em ingles é for nothing.'
+)
